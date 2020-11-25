32ec21c2523e6d8217bf157a47a5be689fe48f9a
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-25 11:08:33 -0700

    All set up is done

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,15 @@
+class CustomersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ db/migrate/20201125180359_add_email_and_phone_to_customer.rb
@@ -0,0 +1,5 @@
+class AddEmailAndPhoneToCustomer < ActiveRecord::Migration[6.0]
+  def change
+    add_column :customers, :phone, :string
+  end
+end
